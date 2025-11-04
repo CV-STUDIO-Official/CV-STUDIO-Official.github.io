e4317d275a49f0f184afffb3ac5e5f5388857afa
ChrisVrellis <chrisvrellis@gmail.com>
2025-11-04 15:18:45 +0200

    ver1.2

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ sections.forEach(section => {
     const sectionTop = section.offsetTop;
     const sectionHeight = section.offsetHeight;
     if (window.scrollY >= sectionTop - 50 && window.scrollY < sectionTop + sectionHeight) {
-        current = section.getAttribute('id'); // Παίρνει το ID της ενότητας, π.χ., "home-section"
+        current = section.getAttribute('id');
     }
     
 });
